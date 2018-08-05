2e70ac534b9816820386463c5df00fa750366037
jiangjiaxin <jiangjx51@163.com>
2018-08-05 23:31:29 +0800

    springMVC整合完成

--- myPlatform/controlModule Maven Webapp/src/main/java/cn/com/ts/controller/system/SUserController.java
+++ myPlatform/controlModule Maven Webapp/src/main/java/cn/com/ts/controller/system/SUserController.java
@@ -1,12 +1,21 @@
 package cn.com.ts.controller.system;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.com.ts.system.model.SUser;
 import cn.com.ts.system.service.SUserService;
 
 @Controller
+@RequestMapping(value="user")
 public class SUserController {
 	
 	@Autowired
@@ -17,9 +26,23 @@ public class SUserController {
 		System.out.println("init SUserController................");
 	}
 	
-	public void page(){
-		SUser user = userService.selectByPrimaryKey("1");
-		System.out.println(user.getChineseName());
+	@RequestMapping(value="", method=RequestMethod.GET)
+	public String index(HttpServletRequest request){
+		return "system/user/index";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="page", method=RequestMethod.POST)
+	public List<SUser> page(SUser model){
+		List<SUser> list = userService.list(model);
+		return list;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="getModelById/{id}", method=RequestMethod.POST)
+	public SUser getModelById(@PathVariable("id") String id){
+		SUser user = userService.selectByPrimaryKey(id);
+		return user;
 	}
 
 	
---
+++ myPlatform/controlModule Maven Webapp/src/main/webapp/WEB-INF/views/system/user/index.jsp
@@ -0,0 +1,53 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
+			+ path + "/";
+%>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<base href="<%=basePath%>">
+
+<title>My JSP 'index.jsp' starting page</title>
+
+<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="expires" content="0">
+<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+<meta http-equiv="description" content="This is my page">
+<script type="text/javascript"
+	src="<%=path%>/resources/js/jquery/jquery-2.1.4.min.js"></script>
+</head>
+
+<body>
+	This is my JSP page.
+	<br><%=path%>
+	<script type="text/javascript">
+	
+		$(function() {
+			var contextPath = getContextPath();
+			$.ajax({
+				url : contextPath + "/user/page",
+				type : "post",
+				dataType : "json",
+				success : function(data){
+					console.info(data);
+				}
+			});
+		})
+		
+		/**
+		 * 获得上下文
+		 * @returns
+		 */
+		function getContextPath() {
+			var pathName = document.location.pathname;
+			var index = pathName.substr(1).indexOf("/");
+			var result = pathName.substr(0, index + 1);
+			return result;
+		}
+	</script>
+</body>
+</html>
